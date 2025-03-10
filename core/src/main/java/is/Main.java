20fe459a472e8b7344297b0a81316da7515904b6
duskill <96116403+duskill@users.noreply.github.com>
2025-03-10 22:08:22 +0100

    Bug fix.

--- core/src/main/java/is/Main.java
+++ core/src/main/java/is/Main.java
@@ -3,11 +3,8 @@ package is;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Point;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.util.Scanner;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,11 +12,11 @@ import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
 
 import is.command.HistoryCommandHandler;
-import is.shapes.controller.GraphicObjectController;
-import is.shapes.model.AbstractGraphicObject;
+import is.interpreter.Interpreter;
 import is.shapes.model.CircleObject;
 import is.shapes.model.ImageObject;
 import is.shapes.model.RectangleObject;
+import is.shapes.model.groups.Group;
 import is.shapes.view.*;
 
 public class Main {
@@ -29,72 +26,82 @@ public class Main {
 		JFrame f = new JFrame();
 
 		JToolBar toolbar = new JToolBar();
-
 		JButton undoButt = new JButton("Undo");
 		JButton redoButt = new JButton("Redo");
 
-		final HistoryCommandHandler handler = new HistoryCommandHandler();
+		HistoryCommandHandler historyHandler = new HistoryCommandHandler();
 
-		undoButt.addActionListener(evt -> handler.undo());
 
-		redoButt.addActionListener(evt -> handler.redo());
+
+		undoButt.addActionListener(evt -> {
+			System.out.println("Tentativo di eseguire undo...");
+			System.out.println("Dimensione storico: " + historyHandler.getHistoryLength()); // Metodo ipotetico, verifica se esiste
+			historyHandler.undo();
+		});
+
+		redoButt.addActionListener(evt -> {
+			System.out.println("Tentativo di eseguire redo...");
+			System.out.println("Dimensione storico redo: " + historyHandler.getRedoLength()); // Metodo ipotetico, verifica se esiste
+			historyHandler.redo();
+		});
 
 		toolbar.add(undoButt);
 		toolbar.add(redoButt);
 
-		final GraphicObjectPanel gpanel = new GraphicObjectPanel();
+		GraphicObjectLogger logger = new GraphicObjectLogger();
+
+		GraphicObjectPanel gpanel = new GraphicObjectPanel();
 
 		gpanel.setPreferredSize(new Dimension(400, 400));
-
 		GraphicObjectViewFactory.FACTORY.installView(RectangleObject.class, new RectangleObjectView());
 		GraphicObjectViewFactory.FACTORY.installView(CircleObject.class, new CircleObjectView());
 		GraphicObjectViewFactory.FACTORY.installView(ImageObject.class, new ImageObjectView());
+		GraphicObjectViewFactory.FACTORY.installView(Group.class, new GroupObjectView());
 
-		AbstractGraphicObject go = new RectangleObject(new Point(180, 80), 20, 50);
 
-		JButton rectButton = new JButton(new CreateObjectAction(go, gpanel, handler));
-		rectButton.setText(go.getType());
-		toolbar.add(rectButton);
 
-		go = new CircleObject(new Point(200, 100), 10);
-		JButton circButton = new JButton(new CreateObjectAction(go, gpanel, handler));
-		circButton.setText(go.getType());
-		toolbar.add(circButton);
 
-		go = new CircleObject(new Point(200, 100), 100);
-		JButton circButton2 = new JButton(new CreateObjectAction(go, gpanel, handler));
-		circButton2.setText("big " + go.getType());
-		toolbar.add(circButton2);
-
-		go = new ImageObject(new ImageIcon(Main.class.getResource("shapes/model/NyaNya.gif")),
-				new Point(240, 187));
-
-		JButton imgButton = new JButton(new CreateObjectAction(go, gpanel, handler));
-		imgButton.setText(go.getType());
-		toolbar.add(imgButton);
-
-		final GraphicObjectController goc = new GraphicObjectController(handler);
-
-		gpanel.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-
-				goc.setControlledObject(gpanel.getGraphicObjectAt(e.getPoint()));
-			}
-		});
 
 		f.add(toolbar, BorderLayout.NORTH);
-		f.add(new JScrollPane(gpanel), BorderLayout.CENTER);
+		f.add(gpanel, BorderLayout.CENTER);
 
 		JPanel controlPanel = new JPanel(new FlowLayout());
 
-		controlPanel.add(goc);
-		f.setTitle("Shapes");
-		f.getContentPane().add(controlPanel, BorderLayout.SOUTH);
+		f.getContentPane().add(new JScrollPane(controlPanel), BorderLayout.SOUTH);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack();
 		f.setVisible(true);
 
+
+		Interpreter interpreter = new Interpreter(historyHandler, gpanel);
+
+		// Lettura dei comandi da console
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Inserisci un comando (oppure 'exit' per uscire):");
+
+		while (true) {
+			System.out.print("> ");
+			String input = scanner.nextLine();
+
+			if (input.equalsIgnoreCase("exit")) {
+				System.out.println("Chiusura del programma.");
+				break;
+			} else if (input.equalsIgnoreCase("undo")) {
+				historyHandler.undo();
+				System.out.println("Undo eseguito.");
+			} else if (input.equalsIgnoreCase("redo")) {
+				historyHandler.redo();
+				System.out.println("Redo eseguito.");
+			} else {
+				try {
+					interpreter.interpret(input);
+					System.out.println("Comando eseguito: " + input);
+				} catch (Exception e) {
+					System.err.println("Errore: " + e.getMessage());
+				}
+			}
+		}
+
+		scanner.close();
 	}
 }
